e64026268f8ab40f5f32c819ec46326f694e84d1
Luiz <luizalex1997@gmail.com>
2018-09-04 18:25:46 -0300

    Falsa Posicao Done

--- CALNUMERICO/falsaposi.js
+++ CALNUMERICO/falsaposi.js
@@ -4,7 +4,7 @@ function draw()
     const expressao = document.getElementById('eq').value
     const expr = math.compile(expressao)
     const xValores = math.range(-100, 100, 1).toArray()
-
+    
     const yValores = xValores.map(function (x) 
     {
         return expr.eval({x: x})
@@ -18,38 +18,49 @@ function draw()
     }
 
     data.push(trace1)
-
+    var xp = 0, xn = 0
     var x0 = Number(document.getElementById('x0').value)
     var x1 = Number(document.getElementById('x1').value)
     const tol = Number(document.getElementById('tol').value)
     const iMax = Number(document.getElementById('epoch').value)
-
-    var i = 2
-    var fx1 = expr.eval({x: x0})
-    var fx2 = expr.eval({x: x1})
-
-    while ( i <= iMax )
+    if(expr.eval({x: x0}) > 0 && expr.eval({x: x1}) < 0 )
     {
-       xk = (x1 - fx1*(x1 - x0))/(fx2 - fx1)
-       if( (xk  - x1) < tol )
-       {
-        var text = "A raiz encontrada foi " + xk + " em " + i + " iterações."
-        console.log("Raiz encontrada")
-        //Exibe o resultado na página HTML
-        document.getElementById("resultado").innerHTML = text
-        break
-       }
-       i += 1
-       k = expr.eval({x: xk})
+        xp = x0 
+        xn = x1
+    }else{
+        xp = x1
+        xn = x0
+    }
 
-       if(k * fx2 < 0 )
-       {
-           x0 = x1
-           fx1 = fx2
-       }
-        x1 = x
-        fx2 = k
-       
+    var i  = 1
+    while ( i <= iMax)
+    {
+        var f1 = expr.eval({x: xp})
+        var f2 = expr.eval({x: xn})
+
+        var xf = ((xp * f2) - (xn * f1)) / (f2 - f1)
+        var fx = expr.eval({x: xf}) 
+
+        var newTrace = {
+            x: [xp, xn],
+            y: [f1, f2],
+            mode: 'lines',
+            type: 'scatter'
+        }
+        data.push(newTrace)
+        if( fx > 0){
+            xp = xf
+        }else{
+            xn = xf
+        }
+        if(math.abs(fx) < tol){
+            var text = "A raiz encontrada foi " + xf + " em " + i + " iterações."
+            console.log("Raiz encontrada")
+            //Exibe o resultado na página HTML
+            document.getElementById("resultado").innerHTML = text
+            break
+        }
+        i += 1
     }
 
     Plotly.newPlot('plot', data)
