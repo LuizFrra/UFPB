f130cd57a4ee81f72e174eb3c2b1fccaad2ebc78
Luiz <luizalex1997@gmail.com>
2019-05-07 19:50:48 -0300

    Mostrando transformada

--- SinaisESistemas/main2.py
+++ SinaisESistemas/main2.py
@@ -20,6 +20,7 @@ def getRealImaginary(imagem):
         return -1
     
     rows, cols = I.shape
+    
     # Aqui voce corta a imagem, os 2 é onde vc quer q comece a cortar e onde quer q termine
     # Só mudar o 2 pelo o que tem no pdf
     crop_img = I[2:rows - 2, 2:cols - 2] # linhas / colunas
@@ -28,15 +29,43 @@ def getRealImaginary(imagem):
     n = cv.getOptimalDFTSize( cols )
     padded = cv.copyMakeBorder(I, 0, m - rows, 0, n - cols, cv.BORDER_CONSTANT, value=[0, 0, 0])
     #padded = cv.copyMakeBorder(I, 0, 92, 0, 112, cv.BORDER_CONSTANT, value=[0, 0, 0])
-    #cv.imshow("image", I)
-    #cv.waitKey(0)
+
     planes = [np.float32(padded), np.zeros(padded.shape, np.float32)]
     complexI = cv.merge(planes)         # Add to the expanded another plane with zeros
     
     cv.dft(complexI, complexI)         # this way the result may fit in the source matrix
     
     cv.split(complexI, planes)                   # planes[0] = Re(DFT(I), planes[1] = Im(DFT(I))
+
+    #       SE NÃO QUISER MOSTRAR A TRANSFORMADA COMENTA DAQUI PRA BAIXO
+
+    cv.magnitude(planes[0], planes[1], planes[0])# planes[0] = magnitude
+    magI = planes[0]
     
+    matOfOnes = np.ones(magI.shape, dtype=magI.dtype)
+    cv.add(matOfOnes, magI, magI) #  switch to logarithmic scale
+    cv.log(magI, magI)
+    
+    magI_rows, magI_cols = magI.shape
+    
+    magI = magI[0:(magI_rows & -2), 0:(magI_cols & -2)]
+    cx = int(magI_rows/2)
+    cy = int(magI_cols/2)
+    q0 = magI[0:cx, 0:cy]         # Top-Left - Create a ROI per quadrant
+    q1 = magI[cx:cx+cx, 0:cy]     # Top-Right
+    q2 = magI[0:cx, cy:cy+cy]     # Bottom-Left
+    q3 = magI[cx:cx+cx, cy:cy+cy] # Bottom-Right
+    tmp = np.copy(q0)               # swap quadrants (Top-Left with Bottom-Right)
+    magI[0:cx, 0:cy] = q3
+    magI[cx:cx + cx, cy:cy + cy] = tmp
+    tmp = np.copy(q1)               # swap quadrant (Top-Right with Bottom-Left)
+    magI[cx:cx + cx, 0:cy] = q2
+    magI[0:cx, cy:cy + cy] = tmp
+    
+    cv.normalize(magI, magI, 0, 1, cv.NORM_MINMAX) # Transform the matrix with float values into a
+    cv.imshow("transformada", magI)
+    cv.waitKey()
+
     return planes
 
 def calculate():
