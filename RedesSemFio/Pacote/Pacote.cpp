e6bc9d681635e7d290c20c13283ac26a876f740e
Luiz <luizalex1997@gmail.com>
2019-08-23 10:20:09 -0300

    Adicionando Algumas Funcionalidades a classe de pacotes

---
+++ RedesSemFio/Pacote/Pacote.cpp
@@ -0,0 +1,59 @@
+#include "Pacote.hpp"
+
+Pacote::Pacote(uint8 Origem[4], uint8 Destino[4], uint8 TTL, std::string Dados)
+{
+    Identificador = 0;
+    OffSet = 0;
+    Flags = 0;
+
+    Versao = 4;
+    TamanhoCabecalho = Checksum = sizeof(Versao) + sizeof(Comprimento) + sizeof(Checksum) + (sizeof(Destino) * 4)
+                            + (sizeof(Origem) * 4) + sizeof(TTL);
+    Comprimento = TamanhoCabecalho + Dados.length;
+    
+};
+
+Pacote::Pacote(uint8 Origem[4], uint8 Destino[4], uint16 Identificador, bool Flags,uint16 OffSet, uint8 TTL, std::string Dados)
+{
+    Versao = 4;
+    TamanhoCabecalho = Checksum = sizeof(Versao) + sizeof(Comprimento) + sizeof(Checksum) + (sizeof(Destino) * 4)
+                            + (sizeof(Origem) * 4) + sizeof(TTL) + sizeof(Identificador) + sizeof(OffSet) + sizeof(Flags);
+    Comprimento = TamanhoCabecalho + Dados.length;
+};
+
+Pacote::~Pacote()
+{
+    delete []Origem;
+    delete []Destino;
+};
+
+bool Pacote::ChecarCheckSum()
+{
+    // If e Else para checar se o pacote é do tipo fragmentado ou não
+    if(OffSet != 0 || (OffSet == 0 && Flags == true))
+    {   
+        // Calcular o atual tamanho do cabeçalho do pacote
+        int currentCheckSum = Checksum = sizeof(Versao) + sizeof(Comprimento) + sizeof(Checksum) + (sizeof(Destino) * 4)
+                            + (sizeof(Origem) * 4) + sizeof(TTL) + sizeof(Identificador) + sizeof(OffSet) + sizeof(Flags);
+        
+        // Checa se é igual ao original
+        if(currentCheckSum == Checksum)
+            return true;
+        else
+            return false;
+    }
+    else
+    {
+        // Calcula o atual tamanho do cabeçalho do pacote
+        int currentCheckSum = Checksum = sizeof(Versao) + sizeof(Comprimento) + sizeof(Checksum) + (sizeof(Destino) * 4)
+                            + (sizeof(Origem) * 4) + sizeof(TTL);
+        
+        // Checa se é igual ao original
+        if(currentCheckSum == Checksum)
+            return true;
+        else
+            return false;
+    }
+    
+};
+
--- RedesSemFio/Pacote/Pacote.hpp
+++ RedesSemFio/Pacote/Pacote.hpp
@@ -3,58 +3,57 @@
 #include <stdint.h>
 #include <string>
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
 class Pacote
 {
+
 private:
     /* data */
     
     // Versão do Protocolo, neste caso será utilizado o IPV4, logo a versão será 4
-    uint8_t Versao;
+    uint8 Versao;
     
-    // Indica o tamanho do cabeçalho, para que seja possível saber onde começa de fato os dados, Pode variar devido
-    // Ao conjunto de opções disponíveis, se um pacote for fragmentado, ira incluir o tamanho do atributo
-    // Flags, offset, Identificador
-    uint32_t TamanhoCabecalho;
+    /* Indica o tamanho do cabeçalho, para que seja possível saber onde começa de fato os dados, Pode variar devido
+       Ao conjunto de opções disponíveis, se um pacote for fragmentado, ira incluir o tamanho do atributo
+       Flags, offset, Identificador */
+    uint32 TamanhoCabecalho;
     
     // Indica o Tamanho de todo o pacote, Incluindo o Cabeçalho
-    uint16_t Comprimento;
+    uint16 Comprimento;
     
     // Campo Utilizado para identificar a qual Conjunto de Pacote o fragmento pertence
-    uint16_t Identificador;
+    uint16 Identificador;
     
     // Indica se o pacote está fragmentado, caso esteja possui valor 1 (menos o último fragmento)
     bool Flags;
     
     // Indica a posição original do fragmento caso seja fragmentado, múltiplo de 8
-    uint16_t OffSet;
+    uint16 OffSet;
     
     // Limitada a vida útil de um pacote, pode ser utilizado para limitar os saltos
-    uint8_t TTL;
+    uint8 TTL;
     
     // Campo de Verificação de cabeçalho, Recalculado em cada roteador
-    uint64_t Checksum;
+    uint64 Checksum;
 
     // Ip de Origem
-    std::string Origem;
+    uint8 Origem[4];
 
     // Iṕ de Destino
-    std::string Destino;
+    uint8 Destino[4];
 
     // Dados
     std::string Dados;
 
 public:
-    Pacote(/* args */);
+    Pacote(uint8 Origem[4], uint8 Destino[4], uint8 TTL, std::string Dados);
+    Pacote(uint8 Origem[4], uint8 Destino[4], uint16 Identificador, bool Flags,uint16 OffSet, uint8 TTL, std::string Dados);
     ~Pacote();
+    bool ChecarCheckSum();
 };
-
-Pacote::Pacote(/* args */)
-{
-}
-
-Pacote::~Pacote()
-{
-}
-
 
 #endif
